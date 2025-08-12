ec508999a123dcd2a2fe7a76f072d07ce075119e
jubo445 <juboraj335.ja@gmail.com>
2025-08-12 21:48:08 +0600

    home screen profile image added

--- app/src/main/java/com/creativeitinstitute/olxcloneanroidapp/views/dashboard/post/PostAddFragment.kt
+++ app/src/main/java/com/creativeitinstitute/olxcloneanroidapp/views/dashboard/post/PostAddFragment.kt
@@ -1,12 +1,22 @@
 package com.creativeitinstitute.olxcloneanroidapp.views.dashboard.post
 
+import android.content.Intent
 import com.creativeitinstitute.olxcloneanroidapp.base.BaseFragment
 import com.creativeitinstitute.olxcloneanroidapp.databinding.FragmentPostAddBinding
+import com.creativeitinstitute.olxcloneanroidapp.views.dashboard.UserDashboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PostAddFragment : BaseFragment<FragmentPostAddBinding>(FragmentPostAddBinding::inflate) {
     override fun setListener() {
+
+
+        with(binding){
+            btnBack.setOnClickListener {
+                startActivity(Intent(requireContext(), UserDashboard::class.java))
+                requireActivity().finish()
+            }
+        }
 
     }
 
